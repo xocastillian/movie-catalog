ecd46749d16a98ad365dc7e2137765afe91e53d6
Timofey <xocastillian@gmail.com>
2025-05-23 10:53:47 +0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	images: {
 		remotePatterns: [
 			{
@@ -13,6 +12,7 @@ const nextConfig: NextConfig = {
 				hostname: 'via.placeholder.com',
 			},
 		],
+		unoptimized: true,
 	},
 }
 
